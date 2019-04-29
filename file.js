e85a06c51356026fce6deb230a5adc6233801a74
Pavel Petrovich <gaara33377@gmail.com>
2019-04-29 21:41:41 +0300

    work with file

---
+++ file.js
@@ -0,0 +1,37 @@
+
+let fs = require('fs'); // for work with files
+
+// --------------FILE DIR-------------- //
+
+ // * __filename -  current file
+ // * __dirname -   dir to file
+
+// ASYNC
+
+// функция срабатывате когда файл будет полностью прочтен асинхронно или синхронно(sync)
+fs.readFile('text2.txt','utf8', function(error, data) {
+
+    if (error) throw error;
+    console.log(data);
+
+});
+
+console.log('async test 1');
+
+let message2 = "Hello async !!!\n";
+
+fs.writeFile('new2.txt', message2, function(error) {
+
+    console.log('error: ' + error);
+    if (error) throw error;
+    let data = fs.readFileSync("new2.txt", "utf8");
+    console.log('data: ' + data);
+});
+
+console.log('async test 2');
+
+ // SYNC
+let file_readed = fs.readFileSync('text.txt', 'utf8'); // read files Sync(синхронно, когда не будет прочитан не вып дальше)
+
+let message = "Hello \n" + file_readed;
+fs.writeFileSync('new.txt', message);
--- server.js
+++ server.js
@@ -2,25 +2,61 @@
 
 let arr = require('./module');
 let events = require('events'); // include event module
+let util = require('util'); // модуль для наследования (классовых) эвентов
 
-// Events
+
+// -----------------util / events---------
+let Cars = function(model){
+
+    this.model = model;
+};
+
+util.inherits(Cars,events.EventEmitter); // устанавливает (наследования) обработчики событий
+
+let bmw = new Cars("BMW");
+let audi = new Cars("audi");
+let volvo = new Cars("volvo");
+
+let cars = [bmw,audi,volvo];
+
+cars.forEach(car => {
+
+    car.on('speed', function(text) {
+
+        console.log(car.model + " speed is - " + text);
+
+    });
+
+});
+
+bmw.emit('speed','254 km');
+
+// ----------------------------------- //
+
+
+// Events object
 
 let myEmit = new events.EventEmitter();
 
 myEmit.on('some_event', function(text){
 
-
+    console.log(text);
 });
 
-// ------------------------------ ///
+myEmit.on('click', function(text){
+
+    console.log(text);
+})
+
+myEmit.emit("some_event", "hello emit!"); // create event
+myEmit.emit("click", "hello click");
+
+// -----------module-------------- ///
 
 console.log(arr.multiply(5,8));
 console.log(arr.array(5));
 
-// --------------------------------------------- //
-
-console.log(__filename); // current file
-console.log(__dirname); // dir to file
+//-----------func--------------------//
 
 let test = function(callback){
 
